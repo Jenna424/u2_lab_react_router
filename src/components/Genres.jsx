07fc3cf726b6980dac4c308a7b67b16ddb784152
Jenna Leopold <jleopold424@gmail.com>
2022-03-18 12:24:22 -0400

    completed step #4

---
+++ src/components/Genres.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import genresArray from './data/genres'
+import { useState, useEffect } from 'react'
+
+const Genres = () => {
+  const [genres, setGenres] = useState([])
+
+  useEffect(() => {
+    setGenres(genresArray)
+  }, [])
+
+  return (
+    <div>
+      <h1>Genres</h1>
+      {genres.map(genre => (
+        <div key={genre.id}>
+          <h3>{genre.name}</h3>
+          <img src={genre.image_background} alt={`${genre.name} image`} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Genres
